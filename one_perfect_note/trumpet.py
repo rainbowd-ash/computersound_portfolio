f2c6c4ec071d5bbbcfb9eb09400e3bb714f78244
ash <ash@ashes.computer>
2024-11-02 22:55:07 -0700

    add fall-off to trumpet

--- one_perfect_note/trumpet.py
+++ one_perfect_note/trumpet.py
@@ -11,28 +11,31 @@ note_frequencies = {
 def generate_trumpet_wave(frequency, duration=1.0, sampling_rate=44100):
     t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
 
-    # brassy
     base_wave = 0.6 * np.sin(2 * np.pi * frequency * t)
     second_harmonic = 0.3 * np.sin(2 * np.pi * 2 * frequency * t)
     third_harmonic = 0.2 * np.sin(2 * np.pi * 3 * frequency * t)
-
-    # harmonics and some noise
     trumpet_wave = base_wave + second_harmonic + third_harmonic
-    trumpet_wave += 0.05 * np.random.normal(0, 1, trumpet_wave.shape)
 
-    # attack, decay
     attack_time = 0.05
     decay_time = 0.1
     sustain_level = 0.8
+    release_time = 0.1
+
     envelope = np.ones_like(t)
-    envelope[:int(sampling_rate * attack_time)] = np.linspace(0, 1, int(sampling_rate * attack_time))
-    envelope[int(sampling_rate * attack_time):int(sampling_rate * (attack_time + decay_time))] = np.linspace(1, sustain_level, int(sampling_rate * decay_time))
-    envelope[int(sampling_rate * (attack_time + decay_time)):] = sustain_level
+    attack_samples = int(sampling_rate * attack_time)
+    decay_samples = int(sampling_rate * decay_time)
+    release_samples = int(sampling_rate * release_time)
+    sustain_samples = len(t) - attack_samples - decay_samples - release_samples
+
+    envelope[:attack_samples] = np.linspace(0, 1, attack_samples)
+    envelope[attack_samples:attack_samples + decay_samples] = np.linspace(1, sustain_level, decay_samples)
+    envelope[attack_samples + decay_samples:attack_samples + decay_samples + sustain_samples] = sustain_level
+    envelope[-release_samples:] = np.linspace(sustain_level, 0, release_samples)
+
     trumpet_wave *= envelope
-    
     return trumpet_wave
 
-parser = argparse.ArgumentParser(description="Play a trumpet sound for a specified note.")
+parser = argparse.ArgumentParser(description="Play a trumpetish sound for a specified note.")
 parser.add_argument("-n", "--note", type=str, default="A4", help="Note to play (e.g., C4, D4, A4) or frequency in Hz. Default is A4.")
 parser.add_argument("-d", "--duration", type=float, default=1.0, help="Duration of the note in seconds. Default is 1 second.")
 
